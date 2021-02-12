6b09f9457f73616ba3c9ada00bfd08aceae57d6e
enrigle <enrigle@gmail.com>
2021-02-12 14:10:27 +0100

    First commit

--- README.md
+++ README.md
@@ -2,4 +2,47 @@
 
 Code for the web app built as part of this tutorial I wrote for Cambridge Spark: https://blog.cambridgespark.com/deploying-a-machine-learning-model-to-the-web-725688b851c7
 
-Can be deployed to Heroku. Example running here: https://bike-model.herokuapp.com/
+#Can be deployed to Heroku. Example running here: https://bike-model.herokuapp.com/
+
+# Structure of the program
+webapp/
+    ├── model/
+    │   └── bike_model_xgboost.pkl
+    ├── templates/
+    │   └── main.html
+    ├── requirements.txt
+    ├── Procfile
+    └── app.py   
+
+# Local testing
+To start up flask on your local machine, make sure you are in the webapp folder and run the command:
+
+*flask run*
+
+# Deployment
+1. Sign up for a free Heroku account at: https://signup.heroku.com/signup/dc
+2. Make sure you have git installed, to push your app to Heroku.
+3. Install the Heroku CLI tool.
+
+After that:
+1. Inside the webapp folder, run the following to create a new repository.
+*git init*
+
+2. Authenticate with Heroku
+*heroku login*
+ 2.1. Press any key to go to the browser
+ 2.2. Log in to the Heroku CLI, then a message will appear: *You can close this page and return to your CLI. It should now be logged in.*, and return to the terminal
+ 2.3. Two messages appear in the terminal:
+    Logging in... done
+    Logged in as name@gmail.com
+
+
+3. Create a new Heroku app
+*heroku create*
+
+4. If you want to choose your own name, simply pass it as an argument. For example:
+*heroku create bike-model*
+
+
+
+ 
--- app.py
+++ app.py
@@ -10,12 +10,14 @@ with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
 app = flask.Flask(__name__, template_folder='templates')
 
 # Set up the main route
+
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
         # Just render the initial form, to get input
         return(flask.render_template('main.html'))
-    
+
     if flask.request.method == 'POST':
         # Extract the input
         temperature = flask.request.form['temperature']
@@ -24,21 +26,23 @@ def main():
 
         # Make DataFrame for model
         input_variables = pd.DataFrame([[temperature, humidity, windspeed]],
-                                       columns=['temperature', 'humidity', 'windspeed'],
+                                       columns=['temperature',
+                                                'humidity', 'windspeed'],
                                        dtype=float,
                                        index=['input'])
 
         # Get the model's prediction
         prediction = model.predict(input_variables)[0]
-    
+
         # Render the form again, but add in the prediction and remind user
         # of the values they input before
         return flask.render_template('main.html',
-                                     original_input={'Temperature':temperature,
-                                                     'Humidity':humidity,
-                                                     'Windspeed':windspeed},
+                                     original_input={'Temperature': temperature,
+                                                     'Humidity': humidity,
+                                                     'Windspeed': windspeed},
                                      result=prediction,
                                      )
+
 
 if __name__ == '__main__':
     app.run()
